b4ef118f31f1d0c197bb4f3d8704abdad8fdb27d
Leonardo Edenak <leonardoedenak@gmail.com>
2025-03-18 19:46:30 +0100

    remove unncessary

--- scripts/basic2.js
+++ scripts/basic2.js
@@ -4,13 +4,9 @@ use('practica');
 db.restaurants.find({type_of_food: "Chinese"})
 
 // Listar las inspecciones con violaciones, ordenadas por fecha.
-// Si se considera warning y fail como violaciones
 db.inspections.find({ 
     result: {result: "Violation Issued"}
   }).sort({date: 1})
 
-// Si unicamente se considera fail como violacion
-db.inspections.find({result: "Fail"}).sort({date: 1})
-
 // Encontrar restaurantes con una calificación superior a 4.
-db.restaurants.find({rating: {$gt: 4}})
+db.restaurants.find({rating: {$gt: 4}}) 

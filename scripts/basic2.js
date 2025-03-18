6c8281e189bdb8bcca180d6bc6eb16ad0994e9ac
Leonardo Edenak <leonardoedenak@gmail.com>
2025-03-18 12:15:44 +0100

    ex2 obligatory done

---
+++ scripts/basic2.js
@@ -0,0 +1,18 @@
+use('practica');
+
+// Buscar todos los restaurantes de un tipo de comida específico (ej. "Chinese").
+db.restaurants.find({type_of_food: "Chinese"})
+
+// Listar las inspecciones con violaciones, ordenadas por fecha.
+// Si se considera warning y fail como violaciones
+db.inspections.find({ 
+    result: { 
+        $in: ["Warning Issued", "Fail"] 
+    }}
+  ).sort({date: 1})
+
+// Si unicamente se considera fail como violacion
+db.inspections.find({result: "Fail"}).sort({date: 1})
+
+// Encontrar restaurantes con una calificación superior a 4.
+db.restaurants.find({rating: {$gt: 4}})

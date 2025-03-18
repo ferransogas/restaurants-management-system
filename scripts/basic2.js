be4e582cd8d198a5451065c8758bacbe2fc9e645
Leonardo Edenak <leonardoedenak@gmail.com>
2025-03-18 19:44:23 +0100

    fix basic2

--- scripts/basic2.js
+++ scripts/basic2.js
@@ -6,10 +6,8 @@ db.restaurants.find({type_of_food: "Chinese"})
 // Listar las inspecciones con violaciones, ordenadas por fecha.
 // Si se considera warning y fail como violaciones
 db.inspections.find({ 
-    result: { 
-        $in: ["Warning Issued", "Fail"] 
-    }}
-  ).sort({date: 1})
+    result: {result: "Violation Issued"}
+  }).sort({date: 1})
 
 // Si unicamente se considera fail como violacion
 db.inspections.find({result: "Fail"}).sort({date: 1})
